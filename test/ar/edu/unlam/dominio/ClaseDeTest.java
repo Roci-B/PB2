7b3ec6b37330ff2da10d96eeab9e12214b38f163
German Mazza <gemazza@unlam.edu.ar>
2025-08-20 12:01:41 -0300

    Se realizaron las pruebas iniciales para la clase gimnasio

--- test/ar/edu/unlam/dominio/ClaseDeTest.java
+++ test/ar/edu/unlam/dominio/ClaseDeTest.java
@@ -2,4 +2,51 @@ package ar.edu.unlam.dominio;
 
 public class ClaseDeTest {
 
+	public void pasosParaDesarrollarConTDD() {
+		// Paso 1:
+		// Escribimos una prueba (Generar clases, metodos y elementos necesarios para
+		// que compile). Ejecutar o correr la prueba y debe dar rojo (la prueba no
+		// pasa).
+
+		// Paso 2:
+		// Agregamos la menor cantidad de codigo productivo (JUSTO y NECESARIO) para que
+		// la prueba pase (de
+		// en color verde).
+
+		// Paso 3:
+		// Re organizamos el codigo (refactor) si es posible y/o util.
+
+		// Importante: Las pruebas, ejecutan metodos. Probamos metodos.
+	}
+
+	public void contenidoDeUnaPrueba() {
+
+		// Preparacion: elementos necesarios para garantizar que la prueba sea efectiva.
+		// Si vamos a sumar 2 + 3, debemos contar con el 2 y el 3.
+
+		// Ejecucion: ejecutamos el metodo que estamos probando. En este ejemplo
+		// deberiamos ejecutar "sumar(2,3)".
+
+		// Verificacion: garantizamos que el dato o resultado OBTENIDO sea el dato o
+		// resultado ESPERADO. El resultado debe ser 5.
+	}
+
+	public void nomenclaturaDePruebas() {
+		// Es importante que en el nombre del metodo que prueba, se incluya la
+		// informacion necesaria para entender que se esta probando.
+
+		// Dado que (given): aclara el contexto.
+		// Cuando (when): aclara lo que se ejecuta.
+		// Entonces (then): aclara el resultado esperado.
+	}
+
+	public void paraPracticar() {
+		// TODO: Realizar las pruebas de una clase Calculadora que permita realizar las
+		// operaciones:
+		// Sumar dos numeros enteros.
+		// Restar dos numeros enteros.
+		// Multiplicar dos numeros enteros.
+		// Dividir dos numeros enteros. El divisor no puede ser 0 (cero). Si asi fuera,
+		// se debe devolver 0 (cero).
+	}
 }
---
+++ test/ar/edu/unlam/dominio/GimnasioTest.java
@@ -0,0 +1,62 @@
+package ar.edu.unlam.dominio;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GimnasioTest {
+	
+	private Gimnasio gimnasioEclipse;
+	
+	@Before
+	public void inicializacion() {
+		// @Before permite instanciar el objeto gimnasio antes de que se corra cada prueba
+		this.gimnasioEclipse = new Gimnasio();
+	}
+
+	@Test
+	public void dadoQueExisteUnGimnasioConNombreCuandoConsultoSuNombreObtengoEclipseGym() {
+		// preparacion:
+		//Gimnasio gimnasioEclipse = new Gimnasio();
+		this.gimnasioEclipse.setNombre("Eclipse Gym");
+
+		// ejecucion:
+		String nombreDelGimnasioObtenido = this.gimnasioEclipse.getNombre();
+
+		// verificacion:
+		String nombreDelGimnasioEsperado = "Eclipse Gym";
+
+		assertEquals(nombreDelGimnasioEsperado, nombreDelGimnasioObtenido);
+	}
+	
+	@Test
+	public void dadoQueExisteUnGimnasioConNombreYCapacidadCuandoConsultoSuCapacidadObtengo50() {
+		// preparacion:
+		//Gimnasio gimnasioEclipse = new Gimnasio();
+		this.gimnasioEclipse.setNombre("Eclipse Gym");
+
+		// ejecucion:
+		Integer capacidadDelGimnasioObtenida = this.gimnasioEclipse.getCapacidad();
+
+		// verificacion:
+		Integer capacidadDelGimnasioEsperado = 50;
+
+		assertEquals(capacidadDelGimnasioEsperado, capacidadDelGimnasioObtenida);
+	}
+
+	@Test
+	public void dadoQueExisteUnGimnasioCuandoInstancioUnObjetoUsandoElConstructorConSuNombreYConsultoElNombreDelGimnasioObtengoEclipseGym() {
+		// preparacion:
+		Gimnasio gimnasioEclipse = new Gimnasio("Eclipse Gym2");
+
+		// ejecucion:
+		String nombreDelGimnasioObtenido = gimnasioEclipse.getNombre();
+
+		// verificacion:
+		String nombreDelGimnasioEsperado = "Eclipse Gym2";
+
+		assertEquals(nombreDelGimnasioEsperado, nombreDelGimnasioObtenido);
+	}
+
+}
