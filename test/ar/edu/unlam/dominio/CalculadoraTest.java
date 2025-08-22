34ef7d6ecf3ddb9a55c258abc9c502708597868d
rocio-bossio-wpp <rocio.bossio@wundermanthompson.com>
2025-08-21 23:00:15 -0300

    Agrego class para el nuevo branch

---
+++ test/ar/edu/unlam/dominio/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.dominio;
+
+public class CalculadoraTest {
+	// TODO: Ejercicio: Realizar las pruebas de una clase Calculadora que permita realizar las
+	// operaciones:
+	// Sumar dos numeros enteros.
+	// Restar dos numeros enteros.
+	// Multiplicar dos numeros enteros.
+	// Dividir dos numeros enteros. El divisor no puede ser 0 (cero). Si asi fuera,
+	// se debe devolver 0 (cero).
+
+}
