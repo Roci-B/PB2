c73701da2bfbdfd0133bdb56850d8dc6fb39156d
rocio-bossio-wpp <rocio.bossio@wundermanthompson.com>
2025-08-26 18:11:59 -0300

    Ejercicio Resuelto

---
+++ src/ar/edu/unlam/dominio/Calculadora.java
@@ -0,0 +1,24 @@
+package ar.edu.unlam.dominio;
+
+public class Calculadora {
+	
+	public Calculadora() {
+	}
+	
+	public int sumar () {
+		return 4+2;
+	}
+
+	public int restar () {
+		return 4-2;
+	}
+
+	public int multiplicar () {
+		return 4*2;
+	}
+
+	public int dividir () {
+		return 4/2;
+	}
+
+}
--- test/ar/edu/unlam/dominio/CalculadoraTest.java
+++ test/ar/edu/unlam/dominio/CalculadoraTest.java
@@ -1,6 +1,48 @@
 package ar.edu.unlam.dominio;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class CalculadoraTest {
+	
+	private Calculadora calculadora;
+
+	@Before
+	public void inicializacion() {
+		this.calculadora = new Calculadora();
+	}
+
+    @Test
+    public void testSumar() {
+    	int resultadoEsperado = 6;
+        int resultadoObtenido = this.calculadora.sumar();
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testRestar() {
+    	int resultadoEsperado = 2;
+        int resultadoObtenido = this.calculadora.restar();
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testMultiplicar() {
+    	int resultadoEsperado = 8;
+        int resultadoObtenido = this.calculadora.multiplicar();
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void testDividir() {
+    	int resultadoEsperado = 2;
+        int resultadoObtenido = this.calculadora.dividir();
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+
 	// TODO: Ejercicio: Realizar las pruebas de una clase Calculadora que permita realizar las
 	// operaciones:
 	// Sumar dos numeros enteros.
